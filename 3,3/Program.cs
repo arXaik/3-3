b52c4183d53ac0f875d7fbdb7d69c7a2da5c5782
arXaik <araik200414@gmail.com>
2022-11-24 19:44:02 +0200

    Добавьте файлы проекта.

---
+++ 3,3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Петросян Араик 3.3 
+    var n = 1;
+var x = 0.3;
+var s = Math.Cos(x);
+var epsilon = 0.0001;
+while (true)
+{
+    n = n + 2;
+    var s1 = s + Math.Sin(n * x) / n;
+    if (Math.Abs(s - s1) < epsilon) break;
+    s = s1;
+    Console.WriteLine(s);
+}
